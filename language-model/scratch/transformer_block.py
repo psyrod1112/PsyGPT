b3d21018dd267bbde03a0b382139e03ea8650fd2
psy <psyrod1112@gmail.com>
2026-08-18 17:58:50 +0900

    feat: 멀티헤드 (멀티헤드(레이어놈(X)) -> FFN(레이어놈(X)))

---
+++ language-model/scratch/transformer_block.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from multihead import MultiHeadAttention
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_dim: int, num_heads: int):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.attn = MultiHeadAttention(embed_dim, num_heads)
+        self.ln2 = nn.LayerNorm(embed_dim)
+        self.ffn = nn.Sequential(nn.Linear(embed_dim, 4*embed_dim),
+                                 nn.GELU(), 
+                                 nn.Linear(4*embed_dim, embed_dim))
+
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.ffn(self.ln2(x))
+        return x
+
+tb = TransformerBlock(16, 4)
+res = tb(torch.randn(2, 5, 16))
+print(res.shape)
